b89d683ac10fb724cf795c2be451bd750265f7e2
unknown <jempsey@gmail.com>
2018-04-26 02:16:08 -0700

    Stretch Project 3 and HTML CSS

---
+++ basecamp-assessment-iii-master/app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer1">
+      <footer>Type Something Above!</footer>
+      <button className="button1">Spin Left</button>
+      <button className="button2">Spin Right</button>
+      </div>
+    );
+  }
+}
---
+++ basecamp-assessment-iii-master/app/src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import Title from "./Header/Title";
+
+export default class Header extends Component {
+  handleChange(e){
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+  render() {
+    return (
+    <div>
+      <header>
+      <Title title={this.props.title} />
+      <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+      </header>
+    </div>
+
+    );
+  }
+}
